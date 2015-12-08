11e4fd7e09905182c38fdc42356aa05b58990be4
groupnyahartoni <anwar.hartoni@gmail.com>
2015-12-08 16:13:04 +0700

    load cart

---
+++ loadcart.php
@@ -0,0 +1,15 @@
+ <div class="modal-dialog">
+
+    <!-- Modal content-->
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal">&times;</button>
+        <h4 class="modal-title">Form Pemesanan</h4>
+      </div>
+      <div class="modal-body">
+ </div>
+      <div class="modal-footer">
+      </div>
+    </div>
+
+  </div>
